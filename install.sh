39cccfe810a402babe74de258af6317e677ad54a
xzessmedia <tim.koepsel@me.com>
2019-09-20 16:47:28 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo Echtwelt RageMP Source Installation
 echo Installing Client Source..
-cd ClientSource && sudo npm install && cd ..
+cd ClientSource && npm install && cd ..
 echo Installing Server Source..
-cd ServerSource && sudo npm install && cd ..
+cd ServerSource && npm install && cd ..
 echo Installing CEF Source..
-cd ClientSource/CEF && sudo npm install && cd ../..
+cd ClientSource/CEF && npm install && cd ../..
 echo Install finished
